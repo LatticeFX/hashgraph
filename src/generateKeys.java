d52e9c6f6115e935ed7298cd62eef9bbfcf9ddc3
bspindler <brian.spindler@gmail.com>
2019-10-27 08:02:04 -0400

    initial project setup

---
+++ src/generateKeys.java
@@ -0,0 +1,16 @@
+import com.hedera.hashgraph.sdk.HederaException;
+import com.hedera.hashgraph.sdk.crypto.ed25519.*;
+
+public class generateKeys {
+
+    public static void main(String[] args) throws HederaException {
+
+        // Generate a Ed25519 private, public key pair
+        var newKey = Ed25519PrivateKey.generate();
+        var newPublicKey = newKey.getPublicKey();
+
+        System.out.println("private key = " + newKey);
+        System.out.println("public key = " + newPublicKey);
+
+    }
+}
